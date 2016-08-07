e0b3800f03f5d43fb0eddf4a2d5609ed57fe9126
Kasra Rahjerdi <contact@kasrarahjerdi.com>
2016-08-07 15:45:48 -0600

    First commit of a Electron/React prototype testbed

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
-TS.app
+hey waddup fam
 
-—
+So this is kinda a mess rn but quick overview wise it's a base Electron app powered by React. When run in dev mode, it sets up a local web server that does nothing but always compile the app's real logic bundle (the React part) every time a file is changed which the Electron base reads.
 
-Let's rename this later
+The React parts are mostly contained in `app/`. The only real Electron stuff rn is `main.js` and `index.html` in the root folder.
+
+To run this locally, you first need to have node/npm installed, I use `nvm` to manage them but you can use whatevver you want.
+
+Once you have node/npm installed, check out this repo then run `npm install` in the checked out folder to download all dependencies.
+
+To actually run the app you can do either `npm start` or `npm run dev`.
+
+`npm start` will compile the React bundle then open up the Electron app.
+`npm dev` will start the live-update server mentioned above and open up the Electron app.
+
+If you start the app with `npm run dev` any changes you make to any files in `app/` will automatically be live-reloaded.
---
+++ app/src/index.jsx
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import electron, { ipcRenderer } from 'electron';
+import moment from 'moment';
+
+import styles from './styles.less'
+
+var TSTextSidebar = React.createClass({
+  render: function() {
+    return (
+      <div className={styles.sidebar}>
+      <ol>
+      {this.props.items.map(function(item, i) {
+        return (
+          <li key={i}>{item.comment}</li>
+        )
+      })}
+      </ol>
+      </div>
+    );
+  }
+});
+
+// #TODO: should it really be a <textarea> or just content-editable?
+var TSTextArea = React.createClass({
+  getInitialState: function() {
+    return {
+      text: "When in the Course of human events[[i stole this tbh]], it becomes necessary for one people to dissolve the political bands which have connected them with another, and to assume among the powers of the earth [[lol can you imagine]], the separate and equal station to which the Laws of Nature and of Nature's God entitle them, a decent respect to the opinions of mankind requires that they should declare the causes which impel them to the separation.",
+      metadata: [],
+    };
+  },
+  componentDidMount: function() {
+    this.updateMetadata()
+  },
+  handleTextChange: function(e) {
+    this.setState({text: e.target.value })
+    this.updateMetadata()
+  },
+  updateMetadata: function() {
+    // #TODO: uhh is this rebuilding the regex each time? who knows!
+    var sidebarSearch = /\[\[(.+?)\]\]/g;
+    // no reason to be rebuilding with each key input but ok for now i guess
+    var metadata = [];
+    var match = sidebarSearch.exec(this.state.text);
+
+    while (match != null) {
+      metadata.push({
+        comment: match[1]
+      })
+
+      match = sidebarSearch.exec(this.state.text);
+    }
+
+    this.setState({metadata: metadata});
+  },
+  render: function() {
+    return (
+      <div className={styles.editorContainer}>
+        <textarea
+          name="mainArea"
+          className={styles.mdTextArea}
+          onChange={this.handleTextChange}
+          value={this.state.text}
+          />
+        <TSTextSidebar
+          items={this.state.metadata}
+          />
+      </div>
+    );
+  }
+})
+
+export default class App extends Component {
+  render() {
+    return (
+      <form className={styles.mainForm}>
+        <TSTextArea />
+      </form>
+    );
+  }
+}
