81a9047ac19b203af508bfbbeaa1f6225184b484
Kasra Rahjerdi <contact@kasrarahjerdi.com>
2016-08-07 20:50:38 -0600

    Start moving over to Draft.js
    It's highlighting the [[comments]] now, but it's not actually
    processing them. I need to switch over to using Entity-s, I think.

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import electron, { ipcRenderer } from 'electron';
-import moment from 'moment';
+import { Editor, EditorState, ContentState, CompositeDecorator, Entity,
+        convertFromRaw, convertToRaw } from 'draft-js';
 
 import styles from './styles.less'
+
+const defaultText = "When in the Course of human events[[i stole this tbh]], it becomes necessary for one people to dissolve the political bands which have connected them with another, and to assume among the powers of the earth [[lol can you imagine]], the separate and equal station to which the Laws of Nature and of Nature's God entitle them, a decent respect to the opinions of mankind requires that they should declare the causes which impel them to the separation.[[also checkout http://butts.com]]";
 
 var CommentsSidebar = React.createClass({
   render: function() {
@@ -20,55 +23,80 @@ var CommentsSidebar = React.createClass({
   }
 });
 
+class InlineNoteComponent extends Component {
+  render() {
+    return (
+      <span className={styles.inlineNote}>{this.props.children}</span>
+    );
+  }
+}
+
 // #TODO: should it really be a <textarea> or just content-editable?
 // #TODO: better names for the components!
-var DETextArea = React.createClass({
-  getInitialState: function() {
-    return {
-      text: "When in the Course of human events[[i stole this tbh]], it becomes necessary for one people to dissolve the political bands which have connected them with another, and to assume among the powers of the earth [[lol can you imagine]], the separate and equal station to which the Laws of Nature and of Nature's God entitle them, a decent respect to the opinions of mankind requires that they should declare the causes which impel them to the separation.",
-      metadata: [],
-    };
-  },
-  componentDidMount: function() {
-    this.updateMetadata()
-  },
-  handleTextChange: function(e) {
-    this.setState({text: e.target.value })
-    this.updateMetadata()
-  },
-  updateMetadata: function() {
-    // #TODO: uhh is this rebuilding the regex each time? who knows!
-    var sidebarSearch = /\[\[(.+?)\]\]/g;
-    // no reason to be rebuilding with each key input but ok for now i guess
-    var metadata = [];
-    var match = sidebarSearch.exec(this.state.text);
+class DETextArea extends Component {
+  constructor(props) {
+    super(props);
 
-    while (match != null) {
-      metadata.push({
-        comment: match[1]
-      })
+    const decorator = new CompositeDecorator([
+      { strategy: noteStrategy, component: InlineNoteComponent }
+    ]);
 
-      match = sidebarSearch.exec(this.state.text);
+    this.state = {
+      editorState: EditorState.createWithContent(ContentState.createFromText(defaultText), decorator),
+      metadata: []
     }
 
-    this.setState({metadata: metadata});
-  },
-  render: function() {
+    this.focusEditor = () => this.refs.mainEditor.focus();
+    this.handleEditorChange = (editorState) => { this.setState({editorState}); this.updateSidebar(); };
+    this.componentDidMount = () => this.updateSidebar();
+  }
+
+  updateSidebar() {
+    // Go through any entities, if their data isn't extracted, extract it
+    const blocks = this.state.editorState.getCurrentContent().getBlocksAsArray();
+    for (var i = 0; i < blocks.length; i++) {
+      blocks[i].findEntityRanges(
+        (character) => {
+          const entityKey = character.getEntity();
+          return true
+        },
+        (start, end) => {
+          console.log(start + ", " + end);
+        }
+      )
+    }
+  }
+
+  render() {
     return (
-      <div className={styles.editorContainer}>
-        <textarea
-          name="mainArea"
-          className={styles.mdTextArea}
-          onChange={this.handleTextChange}
-          value={this.state.text}
-          />
+      <div className={styles.editorRoot} onClick={this.focusEditor}>
+        <div className={styles.editorContainer}>
+          <Editor
+            name="mainArea"
+            ref="mainEditor"
+            onChange={this.handleEditorChange}
+            editorState={this.state.editorState}
+            placeholder="Write something..."
+            />
+        </div>
         <CommentsSidebar
-          items={this.state.metadata}
-          />
+           items={this.state.metadata}
+           />
       </div>
     );
   }
-})
+}
+
+const noteRegex = /\[\[(.+?)\]\]/g;
+
+function noteStrategy(contentBlock, callback) {
+  const text = contentBlock.getText();
+  let matchArr, start;
+  while ((matchArr = noteRegex.exec(text)) !== null) {
+    start = matchArr.index;
+    callback(start, start + matchArr[0].length);
+  }
+}
 
 export default class App extends Component {
   render() {
